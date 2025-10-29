8491d62a05331171f4513370fa7480994c252926
gabislot07 <gabislot07@gmail.com>
2025-10-29 20:02:08 -0300

    Avance 2

---
+++ src/pages/Index.tsx
@@ -0,0 +1,26 @@
+import { LanguageProvider } from "@/contexts/LanguageContext";
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <LanguageProvider>
+      <div className="min-h-screen">
+        <Navbar />
+        <Hero />
+        <About />
+        <Projects />
+        <Skills />
+        <Contact />
+        <Footer />
+      </div>
+    </LanguageProvider>
+  );
+};
+
+export default Index;
